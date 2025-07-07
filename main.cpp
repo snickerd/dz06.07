6337dad3654645fd6870508ea7c93371846e5a0b
safeti <max.bocharov08@mail.ru>
2025-07-07 19:32:16 +0200

    dz07.07

--- main.cpp
+++ main.cpp
@@ -3,105 +3,22 @@
 using namespace std;
 
 int main() {
-
-    /*int n;
-    cin >> n;
+    int n, x;
+    cin >> n >> x;
     int mas[n];
     for (int i = 0; i < n; i++) {
         cin >> mas[i];
     }
-    for (int i = n-1; i >= 0; i--) {
-        cout << mas[i] << " ";
-    }*/
-
-
-
-
-
-    /*int n;
-    int max_pos = 0;
-    int min_pos = 0;
-    cin >> n;
-    int mas[n];
-    for (int i = 0; i < n; i++) {
-        cin >> mas[i];
-    }
-    int max = mas[0];
-    int min = mas[0];
-    for (int i = 0; i < n; i++) {
-        if (mas[i] >= max) {
-            max = mas[i];
-            max_pos = i;
+    for (int i = 0; i < x; i++) {
+        int left;
+        int right;
+        cin >> left >> right;
+        int cnt = 0;
+        for (int j = left; j <= right; j++) {
+            cnt += mas[j-1];
         }
-        if (mas[i] <= min) {
-            min = mas[i];
-            min_pos = i;
-        }
+        cout << cnt << endl;
     }
-    cout << min << " " << min_pos+1 << " " << max << " " << max_pos+1;*/
-
-
-
-
-
-
-
-
-
-
-    /*int n;
-    cin >> n;
-    int mas[n];
-    int chet = 0;
-    int nechet = 0;
-    for (int i = 0; i < n; i++) {
-        cin >> mas[i];
-    }
-    for (int i = 0; i < n; i++) {
-        if (mas[i] % 2 == 0) {
-            chet++;
-        }
-        else {
-            nechet++;
-        }
-    }
-    cout << chet << " " << nechet;*/
-
-
-
-
-    /*int n;
-    int k;
-    cin >> n >> k;
-    int mas[n];
-    for (int i = 0; i < n; i++) {
-        cin >> mas[(i+k)%n];
-    }
-    for (int i = 0; i < n; i++) {
-        cout << mas[i] << " ";
-    }*/
-
-
-
-
-    /*int n;
-    cin >> n;
-    int mas[n];
-    for (int i = 0; i < n; i++) {
-        cin >> mas[i];
-    }
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (mas[j] > mas[j+1]) {
-                int s = mas[j];
-                mas[j] = mas[j+1];
-                mas[j+1] = s;
-            }
-        }
-    }
-    for (int i = 0; i < n; i++) {
-        cout << mas[i] << " ";
-    }*/
 
     return 0;
 }
